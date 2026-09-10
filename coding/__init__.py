97e824150846e696b31968036e97bd1ba8115c1d
dragonballls <123221321+dragonballls@users.noreply.github.com>
2026-09-10 08:58:17 -0700

    feat: expose capability broker primitives

--- coding/__init__.py
+++ coding/__init__.py
@@ -1,3 +1,11 @@
 from coding.ast_utils import find_references, get_function_info, parse_file, rename_symbol
+from coding.capability_broker import (
+    CapabilityBroker,
+    CapabilityDecision,
+    CapabilityLease,
+    CapabilityRequest,
+    CapabilityRule,
+    Decision,
+)
 from coding.indexer import ProjectIndex, get_project_index
 from coding.test_runner import run_format, run_lint, run_tests
