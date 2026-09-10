4621fae8919179a5771748bd6ea3467b2274aab0
dragonballls <123221321+dragonballls@users.noreply.github.com>
2026-09-10 14:32:32 -0700

    Add God's Eye integration package initializer

---
+++ integrations/gods_eye/__init__.py
@@ -0,0 +1,5 @@
+"""God's Eye integration package for Friday."""
+
+from .contract import SpatialContext, SpatialRequest, build_request
+
+__all__ = ["SpatialContext", "SpatialRequest", "build_request"]
